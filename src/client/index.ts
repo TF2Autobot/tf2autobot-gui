77734d4219ae420c7fc31ca04cf7d7640c609b54
Max <maliarmaximilian@gmail.com>
2021-02-18 19:23:10 +0100

    Use vue3 syntax

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,9 +1,7 @@
-import Vue from 'vue'
+import * as Vue from 'vue'
 import index from './vue/index.vue'
 
-new Vue({
-    render: h => h(index)
-}).$mount(
+Vue.createApp(index).mount(
     document.body
 )
 //document.querySelector('html').innerHTML = '<h1 style="text-align: center">Not implemented<h1>';
