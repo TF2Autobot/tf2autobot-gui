10d55e17f440ad4cd99552efeb78aac57ab80067
Max <maliarmaximilian@gmail.com>
2021-02-18 19:24:46 +0100

    Create dependency loader

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,18 @@ import express from 'express';
 import path from 'path';
 const app = express();
 const port = 3000;
+import Twig from 'twig'
+console.log();
+Twig.extendFunction('load_entry_tags', function (entrypoint, type){
+    let entrypoints = require(path.join(require(path.join(process.cwd(), 'webpack.config.js')).output.path, './entrypoints.json')).entrypoints
+    if(!entrypoints[entrypoint]) return'';
+    return entrypoints[entrypoint][type].map((e)=>{
+            return type == 'js' ? `<script src="${e}" defer></script>` : `<link rel="stylesheet" href="${e}">`
+        })
+        .reduce((buffer, e)=>{
+            return buffer + e;
+        }, '');
+})
 
 app.use(express.static('public'));
 
