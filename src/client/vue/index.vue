901b8b58b106d54e79997ddd957d4e8f8da01c24
Max <maliarmaximilian@gmail.com>
2021-02-22 12:16:27 +0100

    Load pricelist

--- src/client/vue/index.vue
+++ src/client/vue/index.vue
@@ -4,8 +4,7 @@
         <message v-for="(msg, index) in this.messages" :key="index" :to-remove=index :msg_type=msg.type @close="msgClose">{{msg.msg}}</message>
         <modal id="test" title="ok"></modal>
         <div class="d-flex">
-            <item :item="{name: 'helloW', enabled: true, style: {image_small: 'https://steamcdn-a.akamaihd.net/apps/440/icons/pile_of_junk2.c5823a4ad9cec27d29ca0ddf049a5af0796ea9bd.png'}, intent: 2, buy: {string: '10Keys'}, sell: {string: '50keys'}}"></item>
-            <item :item="{name: 'helloA', autoprice: true,  style: {image_small: 'https://steamcdn-a.akamaihd.net/apps/440/icons/pile_of_junk2.c5823a4ad9cec27d29ca0ddf049a5af0796ea9bd.png'}, intent: 1, buy: {}, sell: {}}"></item>
+            <item v-for="item in this.pricelist" :item="item"></item>
         </div>
     </div>
 </template>
@@ -14,31 +13,11 @@
 import message from './components/message.vue';
 import modal from './components/modal.vue';
 import item from './components/gridItem.vue';
+import {Pricelist, PricelistItem} from "../../common/types/pricelist";
 
 interface Message {
     msg: String;
     type: "Success"|"Danger"|"Warning"|"info";
-}
-
-interface Item { // Use globally / add missing
-    name: String;
-    buy: {
-        string: String;
-    };
-    sell: {
-        string: String;
-    };
-    style: {
-        image_small: String;
-        effect: String;
-        quality_color: String;
-        craftable: Boolean;
-        border_color: String;
-        killstreak: String;
-    };
-    enabled: Boolean;
-    intent: Number;
-    autoprice: Boolean;
 }
 
 export default {
@@ -50,7 +29,8 @@ export default {
     data() {
         return {
             error_messages: [] as Message[],
-            items: [] as Item[]
+            items: [] as PricelistItem[],
+            pricelist: [] as Pricelist
         }
     },
     methods: {
@@ -59,7 +39,7 @@ export default {
         },
         loadItems() {
             //TODO IMPLEMENT IN BACKEND
-           /*fetch('/getItems')
+           fetch('/pricelist')
                 .then((response) => {
                     return response.json();
                 })
@@ -68,7 +48,7 @@ export default {
                 })
                 .catch((error) => {
                     console.error('Error:', error);
-                });*/
+                });
         }
     },
     created() {
