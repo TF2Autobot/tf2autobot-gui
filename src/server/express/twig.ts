52c61c2bdad30a35ea5bd975b57b7c24e729370d
Max <maliarmaximilian@gmail.com>
2021-02-26 22:19:18 +0100

    removed todo, done by require automatically

--- src/server/express/twig.ts
+++ src/server/express/twig.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export = function init(app: Express): void {
     Twig.extendFunction('load_entry_tags', function (entrypoint, type){
-        let entrypoints = require(path.join(require(path.join(process.cwd(), 'webpack.config.js')).output.path, './entrypoints.json')).entrypoints // TODO: cache so we are not loading files with every access
+        let entrypoints = require(path.join(require(path.join(process.cwd(), 'webpack.config.js')).output.path, './entrypoints.json')).entrypoints
         if(!entrypoints[entrypoint] || !entrypoints[entrypoint][type]) return'';
         return entrypoints[entrypoint][type].map((e)=>{
             return type == 'js' ? `<script src="${e}" defer></script>` : `<link rel="stylesheet" href="${e}">`
