3fa17c23d7d367da2d3dad245e7d24a831a66f5b
Max <maliarmaximilian@gmail.com>
2021-02-26 18:31:24 +0100

    dark mode pt 2

--- src/client/vue/components/modal.vue
+++ src/client/vue/components/modal.vue
@@ -2,14 +2,14 @@
     <div class="modal fade" :id=id tabindex="-1" :aria-labelledby="id + 'Label'" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header bg-dark">
                     <h5 class="modal-title" :id="id + 'Label'">{{title}}</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body bg-dark">
                     <slot name="body"></slot>
                 </div>
-                <div class="modal-footer">
+                <div class="modal-footer bg-dark">
                     <slot name="footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     </slot>
