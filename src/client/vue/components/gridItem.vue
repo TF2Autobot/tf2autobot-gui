f2efe5e65a162d86660428f998ca503fefe2f3e4
Maximilián Maliar <maliarmaximilian@gmail.com>
2022-03-19 22:38:16 +0100

    add count, untested

--- src/client/vue/components/gridItem.vue
+++ src/client/vue/components/gridItem.vue
@@ -12,6 +12,9 @@
         <svg class="bi bi-check item-check" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" v-else-if="selected">
             <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
         </svg>
+        <div v-if="item.amount" class="item-count">
+            <b>{{item.amount}}X</b>
+        </div>
         <div class="ks">
             <b>{{item.style.killstreak}}</b>
         </div>
@@ -115,5 +118,11 @@ $dark: #212529; //From bootstrap
     z-index: 2;
     color: $dark;
     @extend %unselectable;
+}
+.item-count {
+    text-align: right;
+    position: absolute;
+    bottom: 0px;
+    right: 5px;
 }
 </style>
