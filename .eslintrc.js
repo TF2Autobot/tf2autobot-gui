a50a67d2998a9d48d87dac7313132aee81364693
Maximilián Maliar <maliarmaximilian@gmail.com>
2022-05-26 20:01:39 +0200

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,46 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	env: {
-		commonjs: true,
-		es6: true,
-		node: true
-	},
-	extends: [],
-	globals: {
-		Atomics: "readonly",
-		SharedArrayBuffer: "readonly"
-	},
-	parserOptions: {
-		project: './tsconfig.eslint.json'
-	},
-	rules: {
-		"indent": ["error", "tab"],
-		"no-tabs": 0,
-		"max-len": 0,
-		"linebreak-style": 0,
-		"quote-props": ["warn", "consistent-as-needed", {
-			numbers: false
-		}],
-		"no-trailing-spaces": ["error", {
-			skipBlankLines: true,
-			ignoreComments: true
-		}],
-		"comma-dangle": ["error", "never"],
-		"require-jsdoc": 2,
-		"space-before-function-paren": ["error", {
-			anonymous: "never",
-			named: "never",
-			asyncArrow: "always"
-		}],
-		"curly": ["warn", "all"], // May be ignored if its a single line statement with an immediate return / continue / break. Otherwise follow this rule.
-		"object-curly-spacing": ["error", "always"],
-		"object-property-newline": ["error", {
-			allowAllPropertiesOnSameLine: false
-		}],
-		"new-cap": 0,
-		"no-useless-catch": "error",
-		"arrow-spacing": 2,
-		"camelcase": "error"
-	}
+    root: true,
+    parser: '@typescript-eslint/parser',
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    extends: [],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+        project: './tsconfig.eslint.json'
+    },
+    rules: {
+        'indent': ['error', 4],
+        'no-tabs': 0,
+        'max-len': 0,
+        'linebreak-style': 0,
+        'quote-props': ['warn', 'consistent-as-needed', {
+            numbers: false
+        }],
+        'no-trailing-spaces': ['error', {
+            skipBlankLines: true,
+            ignoreComments: true
+        }],
+        'comma-dangle': ['error', 'never'],
+        'require-jsdoc': 2,
+        'space-before-function-paren': ['error', {
+            anonymous: 'never',
+            named: 'never',
+            asyncArrow: 'always'
+        }],
+        'curly': ['warn', 'all'], // May be ignored if its a single line statement with an immediate return / continue / break. Otherwise follow this rule.
+        'object-curly-spacing': ['error', 'always'],
+        'object-property-newline': ['error', {
+            allowAllPropertiesOnSameLine: false
+        }],
+        'new-cap': 0,
+        'no-useless-catch': 'error',
+        'arrow-spacing': 2,
+        'camelcase': 'error'
+    }
 };
