1153713cd4addba7b680134453d09fd375d2cd8d
PesAdam <pesadam15@gmail.com>
2022-07-05 22:55:15 +0200

    depth for indentation

--- src/client/vue/config.vue
+++ src/client/vue/config.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
         <h1> config </h1>
-        <ul>
+        <ul v-for="item in items"
+            :key = item
+            :style="{'margin-left': `${depth * 20}px`}">
             <li>
-                <tree-browser :node="data" />
-            </li>
+                {{item}}
+             </li>
         </ul>
       </div>
 </template>
@@ -19,8 +21,10 @@
         name: "config.vue",
         data(){
             return{
-                data: {},
                 index: 0,
+                typ: "",
+                items: [{}],
+                depth: 0,
             };
         },
         components:{
@@ -28,35 +32,50 @@
         },
         methods: {
                 getJson(){
-                    fetch('https://vue-http-demo-2070c-default-rtdb.europe-west1.firebasedatabase.app/options.json')
+                    fetch('https://adams-demo-default-rtdb.europe-west1.firebasedatabase.app/options.json')
                         .then((response) => {
                             return response.json();
                         })
                         .then((data) => {
-                            this.data = data;
-                            this.flatJson(data);
+                            this.flatJson(data, '');
+                            this.runThruData(data);
                         })
                         .catch((error) => {
                             console.error('Error: ', error);
                         });
                 },
-                flatJson(entry:object){
+                flatJson(entry:object, parrent:string){  
                     for(const [key, item] of Object.entries(entry)){
                         this.index ++;
                         if(typeof item  === 'object'){
-                            console.log(key);
-                            this.flatJson(item);    
+                            this.flatJson(item, key);  
                         } else {
-                            console.log(this.index + ' : ' + key+' : '+ item);
-                        }    
+                            // if(typeof item === 'number'){
+                            //     this.typ = "cislo";
+                            // } else if (typeof item === 'string'){
+                            //     this.typ = "text"
+                            // }
+                            
+                            this.items.push(parrent); 
+                            console.log(parrent + ' ' + this.index + ' : ' + key+' : '+ item + ' : ' + this.typ);
+                        }     
                     }
-                    
-                }
-
+                },
+                runThruData(jsonObject:object){
+                    for(const [key, item] of Object.entries(jsonObject)){
+                        if (typeof item === 'object'){
+                            console.log(this.depth+ ':'+key);
+                            this.depth++;
+                            this.runThruData(item);
+                        } else {
+                            console.log(this.depth+ ':->' +key+' : '+item)
+                        }
+                    }
+                this.depth--; 
+                },
         },
         mounted(){
             this.getJson();
-
         },
     };
 </script>
