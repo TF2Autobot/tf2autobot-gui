6ea3723935e2dfcad18dd97b69973a70d3b43f84
Maximilián Maliar <maliarmaximilian@gmail.com>
2022-07-27 21:34:07 +0200

    rework config

--- src/client/vue/config.vue
+++ src/client/vue/config.vue
@@ -1,43 +1,16 @@
 <template>
     <div class="container">
-        <h1> config </h1>
-
-        <div v-for="item in items.slice(1)"
-            :key="item.id"
-            :id="item.id"
-            class="row"
-        >
-
-            <h5  :style="{'margin-left': `${item.depth * 30}px`}" class="col-sm lh-lg">
-                {{item.key}} <br>
-            </h5>
-
-            <h5 class="col-sm text-muted lh-lg">
-                {{item.data}}
-            </h5>
-            
-            <div class="col">
-                <div v-if="item.type === 'checkbox'">
-                    <input type="checkbox" v-model="item.data" />
-                </div>
-                
-                <div v-else-if="item.type === 'number'">
-                    <input type="number" v-model="item.data" />
-                </div>
-
-                <div v-else-if="item.type === 'text'">
-                    <input type="text" v-model="item.data" />
-                </div>
+        <form method="post" target="/config">
+            <h1>Config</h1>
+            <div v-for="(value, key) of options"
+                 :key="key">
+                <recursive-option :for="key" :level="2" :data="value"></recursive-option>
             </div>
-
-        </div>
-
-        <div class="position-relative">
-            <div class="position-fixed bottom-30 start-90">
-                <button type="button" class="btn btn-success">Send</button>
+            <div class="text-center">
+                <input type="submit" value="Save Changes">
             </div>
-        </div>
-      </div>
+        </form>
+    </div>
 </template>
 
 <style>
@@ -45,71 +18,43 @@
 </style>
 
 <script lang="ts">
-    export default {
-      name: "config.vue",
-        data(){
-            return{
-                index: 0,
-                depth: 0,
-                items: [{}],
-            };
+import recursiveOption from './components/recursiveOption.vue';
+export default {
+    name: "config.vue",
+    components: {
+        recursiveOption
+    },
+    data(){
+        return{
+            options: {}
+        };
+    },
+    methods: {
+        getJson(){
+            fetch('https://adams-demo-default-rtdb.europe-west1.firebasedatabase.app/options.json')
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    this.options = data;
+
+                })
+                .catch((error) => {
+                    console.error('Error: ', error);
+                });
         },
-        methods: {
-                getJson(){
-                    fetch('https://adams-demo-default-rtdb.europe-west1.firebasedatabase.app/options.json')
-                        .then((response) => {
-                            return response.json();
-                        })
-                        .then((data) => {
-                            this.runThruData(data);
-                            
-                        })
-                        .catch((error) => {
-                            console.error('Error: ', error);
-                        });
-                },
-                runThruData(jsonObject:object){
-                    for(const [key, item] of Object.entries(jsonObject)){     
-                       if (typeof item === 'object'){
-                            this.items.push({
-                                key: key, 
-                                depth: this.depth, 
-                                type: "none",
-                                data: ""
-                            });
-                            this.depth++;
-                            this.runThruData(item);
-                        } else {
-                            this.items.push({
-                                key: key, 
-                                depth: this.depth, 
-                                type: this.whichType(item),
-                                data: item
-                        });
-                        }
-                    }
-                    this.depth--; 
-                },
-                whichType(type:boolean):string{
-                    if(type === false || type === true){
-                        return 'checkbox';
-                    }else if(typeof type === 'number'){
-                        return 'number';
-                    }
-                    else {
-                        return 'text';
-                    }
-                },
-        },
-        
-        mounted(){
-            this.getJson();
-        },
-    };
-</script>   
+    },
+
+    mounted(){
+        this.getJson();
+    },
+};
+</script>
 
 
-<style scoped>
-    
-
+<style scoped lang="scss">
+    input[type=submit] {
+        position: fixed;
+        bottom: 10px;
+    }
 </style>
--- src/server/routes/config.ts
+++ src/server/routes/config.ts
@@ -1,9 +1,5 @@
-import express, {Router} from 'express';
+import express, { Router } from 'express';
 import SchemaManager from "@tf2autobot/tf2-schema";
-const router = express.Router();
-import * as trades from '../app/trades';
-import fs from 'fs-extra';
-import paths from '../config/paths';
 import BotConnectionManager from "../IPC";
 
 export default function config(schemaManager: SchemaManager, botManager: BotConnectionManager): Router {
