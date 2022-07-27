f94e5920e8edaa237dc1fba4802a44ca7aa52436
Maximilián Maliar <maliarmaximilian@gmail.com>
2022-07-27 21:34:31 +0200

    Revert "development, TODO: revert"
    This reverts commit 02f0c10b3010f0f23ba69f27605df1489c38c651.

--- src/server/express/init.ts
+++ src/server/express/init.ts
@@ -5,11 +5,9 @@ import initNjs from './nunjucks';
 import initRoutes from './routes';
 import SchemaManager from "@tf2autobot/tf2-schema";
 import type BotConnectionManager from "../IPC";
-import config from "../routes/config";
 
 export = function init(app: Express, schemaManager: SchemaManager, botManager: BotConnectionManager): void {
     app.use(express.static('./public'))
-    app.use('/', config(schemaManager, botManager))
     initBody(app);
     if(process.env.STEAM_AUTH) {
         initPassport(app, botManager);
